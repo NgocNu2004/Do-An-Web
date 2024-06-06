c4548a7595209896f39097e33f14381788841edb
Nu <nu022101027@tgu.edu.vn>
2024-06-06 22:07:52 +0700

    update

--- Thiwed_main/index.js
+++ Thiwed_main/index.js
@@ -4,21 +4,26 @@ var btnleft=document.querySelector(".btn-left");
 var btnright=document.querySelector(".btn-right");
 var index=0;
 var widthOfImage = listImages[0].offsetWidth;
-
+var idSetInterval;
 var handlechangeslide=()=>{
-    if(index>=listImages.length) index=0;
-    slide.style.transform=`translateX(${widthOfImage*-1*index++}px)`;
+    idSetInterval = setInterval(()=>{
+        if(index>=listImages.length) index=0;
+        slide.style.transform=`translateX(${widthOfImage*-1*index++}px)`;
+    }, 1000)
 }
 
-
-
+handlechangeslide();
 btnright.addEventListener('click',()=>{
+    clearInterval(idSetInterval);
     if(index==listImages.length-1) index=-1;
     slide.style.transform=`translateX(${widthOfImage*-1*++index}px)`;
+    handlechangeslide();
 })
 btnleft.addEventListener('click',()=>{
+    clearInterval(idSetInterval);
     if(index==0) index=listImages.length;
     slide.style.transform=`translateX(${widthOfImage*-1*--index}px)`;
+    handlechangeslide();
 })
 //ok
 //call mess
